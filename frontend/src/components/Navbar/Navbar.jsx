1528061d750a2c3f73ba96e602248354e31d3089
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2025-01-16 20:05:06 +0530

    initial updates

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='Navbar'>Navbar</div>
+    <div className='Navbar'>
+     <img src={assets.logo} alt="Company Logo" className='logo' />
+
+
+    </div>
   )
 }
 
