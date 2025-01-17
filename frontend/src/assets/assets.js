baa212733ddfc8b8049c867e99c0148c1620ac1c
aman-7248 <127982352+aman-7248@users.noreply.github.com>
2025-01-17 13:31:10 +0530

    navbar work done

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,4 +1,10 @@
-import logo from './logo.png'
+import logo from './logo.jpg'
+import search_icon from './search_icon.png'
+import basket_icon from './basket_icon.png'
+
+
 export const assets={
     logo,
+    search_icon,
+    basket_icon,
 };
